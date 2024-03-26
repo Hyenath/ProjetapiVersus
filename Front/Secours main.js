c01b4c3d46b367fe67290743a4505763d5881d0e
Nathan <144818587+Hyenath@users.noreply.github.com>
2024-03-26 16:27:29 +0100

    Create Secours main.js

---
+++ Front/Secours main.js
@@ -0,0 +1,42 @@
+// Parcourir les données et ajouter des options au menu déroulant
+        /*data.forEach(item => {
+            let option = document.createElement("option");
+            let option2 = document.createElement("option");
+            let option3 = document.createElement("option");
+            let option4 = document.createElement("option");
+
+            option.value = item.nom;
+            option.text = item.nom;
+            option.setAttribute('forcex', item.force); // Ajouter un attribut forcex à l'option
+            option.setAttribute('vitessex', item.vitesse);
+            option.setAttribute('defensex', item.defense);
+            option.setAttribute('durabilitex', item.durabilité);
+            option.setAttribute('intelligencex', item.intelligence);
+
+            option2.value = item.nom;
+            option2.text = item.nom;
+            option2.setAttribute('forcex2', item.force);  // Ajouter un attribut forcex2 à l'option
+            option2.setAttribute('vitessex2', item.vitesse);
+            option2.setAttribute('defensex2', item.defense);
+            option2.setAttribute('durabilitex2', item.durabilité);
+            option2.setAttribute('intelligencex2', item.intelligence);
+            
+
+            option3.value = item.nomA;
+            option3.text = item.nomA;
+            option3.setAttribute('bonusx', item.bonus);  // Ajouter un attribut bonusx à l'option
+            option3.setAttribute('malusx', item.malus);
+            option3.setAttribute('effectsx', item.effects);
+
+            option4.value = item.nomA;
+            option4.text = item.nomA;
+            option4.setAttribute('bonusx2', item.bonus);  // Ajouter un attribut bonusx2 à l'option
+            option4.setAttribute('malusx2', item.malus);
+            option4.setAttribute('effectsx2', item.effects);
+
+           
+            select.appendChild(option);
+            select2.appendChild(option2);
+            select3.appendChild(option3);
+            select4.appendChild(option4);
+        });*/
