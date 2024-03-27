50a0881c3e489fa15aa787b56a687b1fb49127de
Nathan <144818587+Hyenath@users.noreply.github.com>
2024-03-27 11:40:19 +0100

    Update main.js

--- Front/main.js
+++ Front/main.js
@@ -32,47 +32,34 @@ async function fetchData() {
         // Parcourir les données et ajouter des options au menu déroulant
         data.forEach(item => {
             let option = document.createElement("option");
-            let option2 = document.createElement("option");
-            let option3 = document.createElement("option");
-            let option4 = document.createElement("option");
-
             option.value = item.nom;
             option.text = item.nom;
-            option.setAttribute('forcex', item.force); // Ajouter un attribut forcex à l'option
+            option.setAttribute('forcex', item.forcetest);
             option.setAttribute('vitessex', item.vitesse);
             option.setAttribute('defensex', item.defense);
             option.setAttribute('durabilitex', item.durabilité);
             option.setAttribute('intelligencex', item.intelligence);
-
-            option2.value = item.nom;
-            option2.text = item.nom;
-            option2.setAttribute('forcex2', item.force);  // Ajouter un attribut forcex2 à l'option
-            option2.setAttribute('vitessex2', item.vitesse);
-            option2.setAttribute('defensex2', item.defense);
-            option2.setAttribute('durabilitex2', item.durabilité);
-            option2.setAttribute('intelligencex2', item.intelligence);
-            
-
+        
+            let option2 = option.cloneNode(true);
+        
+            let option3 = document.createElement("option");
             option3.value = item.nomA;
             option3.text = item.nomA;
-            option3.setAttribute('bonusx', item.bonus);  // Ajouter un attribut bonusx à l'option
+            option3.setAttribute('bonusx', item.bonus);
             option3.setAttribute('malusx', item.malus);
             option3.setAttribute('effectsx', item.effects);
-
-            option4.value = item.nomA;
-            option4.text = item.nomA;
-            option4.setAttribute('bonusx2', item.bonus);  // Ajouter un attribut bonusx2 à l'option
-            option4.setAttribute('malusx2', item.malus);
-            option4.setAttribute('effectsx2', item.effects);
-
-           
+        
+            let option4 = option3.cloneNode(true);
+        
             select.appendChild(option);
             select2.appendChild(option2);
             select3.appendChild(option3);
             select4.appendChild(option4);
         });
-
         
+      
+
+
     } catch (error) {
         // Gestion des erreurs ici
         console.error('Erreur lors de la récupération des données de l\'API:', error);
@@ -91,19 +78,28 @@ document.getElementById('button1').addEventListener('click', function (event) {
     let select1Value = document.getElementById('choix').value;
     let select2Value = document.getElementById('choix2').value;
     let select3Value = document.getElementById('choix').options[document.getElementById('choix').selectedIndex].getAttribute('forcex');
-    let select4Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('forcex2');
+    let select4Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('forcex');
     let select5Value = document.getElementById('choix').options[document.getElementById('choix').selectedIndex].getAttribute('vitessex');
-    let select6Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('vitessex2');
+    let select6Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('vitessex');
     let select7Value = document.getElementById('choix').options[document.getElementById('choix').selectedIndex].getAttribute('defensex');
-    let select8Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('defensex2');
+    let select8Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('defensex');
     let select9Value = document.getElementById('choix').options[document.getElementById('choix').selectedIndex].getAttribute('durabilitex');
-    let select10Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('durabilitex2');
+    let select10Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('durabilitex');
     let select11Value = document.getElementById('choix').options[document.getElementById('choix').selectedIndex].getAttribute('intelligencex');
-    let select12Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('intelligencex2');
-    afficherResultat(select1Value, select2Value, select3Value, select4Value, select5Value, select6Value, select7Value, select8Value, select9Value, select10Value, select11Value, select12Value);
+    let select12Value = document.getElementById('choix2').options[document.getElementById('choix2').selectedIndex].getAttribute('intelligencex');
+
+    let select13Value = document.getElementById('choixA1').value;
+    let select14Value = document.getElementById('choixA2').value;
+    let select15Value = document.getElementById('choixA1').options[document.getElementById('choixA1').selectedIndex].getAttribute('Bonusx');
+    let select16Value = document.getElementById('choixA2').options[document.getElementById('choixA2').selectedIndex].getAttribute('Bonusx');
+    let select17Value = document.getElementById('choixA1').options[document.getElementById('choixA1').selectedIndex].getAttribute('Malusx');
+    let select18Value = document.getElementById('choixA2').options[document.getElementById('choixA2').selectedIndex].getAttribute('Malusx');
+    let select19Value = document.getElementById('choixA1').options[document.getElementById('choixA1').selectedIndex].getAttribute('Effectsx');
+    let select20Value = document.getElementById('choixA2').options[document.getElementById('choixA2').selectedIndex].getAttribute('Effectsx');
+    afficherResultat(select1Value, select2Value, select3Value, select4Value, select5Value, select6Value, select7Value, select8Value, select9Value, select10Value, select11Value, select12Value, select13Value, select14Value, select15Value, select16Value, select17Value, select18Value, select19Value, select20Value);
 });
 
-function afficherResultat(value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11, value12) {
+function afficherResultat(value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11, value12, value13, value14, value15, value16, value17, value18, value19, value20) {
     document.getElementById('name1').textContent = value1;
     document.getElementById('name2').textContent = value2;
     document.getElementById('force1').textContent = value3;
@@ -116,12 +112,21 @@ function afficherResultat(value1, value2, value3, value4, value5, value6, value7
     document.getElementById('durabilite2').textContent = value10;
     document.getElementById('intelligence1').textContent = value11;
     document.getElementById('intelligence2').textContent = value12;
+
+    document.getElementById('nameA1').textContent = value13;
+    document.getElementById('nameA2').textContent = value14;
+    document.getElementById('bonus1').textContent = value15;
+    document.getElementById('bonus2').textContent = value16;
+    document.getElementById('malus1').textContent = value17;
+    document.getElementById('malus2').textContent = value18;
+    document.getElementById('effects1').textContent = value19;
+    document.getElementById('effects2').textContent = value20;
     
-// Appeler la fonction pour récupérer les données des personnages à chaque fois que le bouton est cliqué
+    // Appeler la fonction pour récupérer les données des personnages à chaque fois que le bouton est cliqué
 
     let image1 = document.getElementById('image1');
     let image2 = document.getElementById('image2');
-    console.log(value1);
+    console.log(value1, value2, valueA1, valueA2);
     if (value1 === "batman") {
         image1.src = "Images/batman-wallpapers-574x1024.jpg";
     } else if (value1 === "choix2") {
